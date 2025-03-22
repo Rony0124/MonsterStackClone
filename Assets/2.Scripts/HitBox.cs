4c7d338baa58c6a4e15bb6a5920a1199e2dbb838
rony <siamore9724@gmail.com>
2025-03-22 14:13:27 +0900

    layer 이름 수정

--- Assets/2.Scripts/HitBox.cs
+++ Assets/2.Scripts/HitBox.cs
@@ -7,23 +7,8 @@ public class HitBox : MonoBehaviour
 
    private void OnTriggerEnter2D(Collider2D other)
    {
-      Debug.Log(other.gameObject.name + " Triggered");
       if(layerMask == (layerMask | (1 << other.gameObject.layer)))
       {
-         var hurtBox = other.transform.GetComponent<HurtBox>();
-         if (hurtBox != null)
-         {
-            hurtBox.TakeHit();
-         }
-      }
-   }
-
-   public void OnCollisionEnter2D(Collision2D other)
-   {
-      Debug.Log(other.gameObject.name + " collision");
-      if(layerMask == (layerMask | (1 << other.gameObject.layer)))
-      {
-       //  Debug.Log(other.gameObject.name + " collision");
          var hurtBox = other.transform.GetComponent<HurtBox>();
          if (hurtBox != null)
          {
